9e7c6f577c7a3fde4bdc5df23b5cfb64bc674861
Shubham Gupta <shubham.gupta@splashlearn.com>
2026-01-04 21:50:33 +0530

    minor refactor

--- src/main/java/repl/commands/builtin/PwdCommand.java
+++ src/main/java/repl/commands/builtin/PwdCommand.java
@@ -3,15 +3,14 @@ package repl.commands.builtin;
 import repl.commands.Command;
 import repl.exceptions.ReplException;
 
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
 public class PwdCommand implements Command {
 	@Override
 	public String execute(String originalInput, String mainCommandStr, List<String> args) throws ReplException {
-		Path currentDirRelativePath = Paths.get("");
-		Path currentDirAbsolutePath = currentDirRelativePath.toAbsolutePath();
-		return currentDirAbsolutePath.toString();
+		return Paths.get("")
+				.toAbsolutePath()
+				.toString();
 	}
 }
